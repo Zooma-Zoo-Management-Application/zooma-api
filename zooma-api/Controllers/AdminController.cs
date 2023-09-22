27f12676cbcac2555627bb9fc1c56cf79f8e7b07
hmtken <143521414+hmtken@users.noreply.github.com>
2023-09-22 16:48:59 +0700

    Feature/login (#6)
    * Add login function and config Auto mapping.
    
    * Login + jwt token and method to get data
    
    * Update DateTimeTrackerController (wrong)
    
    * Update DateTimeController with body Post
    
    * Authorization function and template

---
+++ zooma-api/Controllers/AdminController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using zooma_api.Models;
+
+namespace zooma_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase // TEST AUTHORIZE ADMIN
+    {
+        private readonly ZoomaContext _context = new ZoomaContext();
+
+        //For admin Only
+        [HttpGet]
+        [Route("admins")]
+        [Authorize(Roles = "Admin")]
+        public IActionResult AdminEndpoint()
+        {
+            var currentUser = GetCurrentUser(); 
+           // var role = currentUser.RoleId == 1 ? "admin" : null;
+            return Ok($"Hi you are an admin ");
+        }
+        private User  GetCurrentUser()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+                var email = userClaims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
+                var result =  _context.Users.FirstOrDefault(x => x.Email == email);
+                return result;
+            }
+            return null;
+        }
+
+        
+    }
+}
