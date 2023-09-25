fa4cc42395bddef4c042577fab15c3d6837f2653
hmtken <minhtien11052003@gmail.com>
2023-09-26 00:19:19 +0700

    Add Sign up and Update APIS

---
+++ zooma-api/DTO/UpdateBody.cs
@@ -0,0 +1,13 @@
+﻿namespace zooma_api.DTO
+{
+    public class UpdateUserBody
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Gender { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string AvatarUrl { get; set; }
+    }
+}
---
+++ zooma-api/DTO/UpdatePasswordBody.cs
@@ -0,0 +1,10 @@
+﻿namespace zooma_api.DTO
+{
+    public class UpdatePasswordBody
+    {
+        public short Id { get; set; }
+
+        public string currentPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
