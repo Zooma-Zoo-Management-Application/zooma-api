664d47b9aa37a95bfa3b99d2f81ad26fbfc1a278
hmtken <minhtien11052003@gmail.com>
2023-09-26 18:49:01 +0700

    Update Launch Apis

--- zooma-api/Controllers/UsersController.cs
+++ zooma-api/Controllers/UsersController.cs
@@ -298,9 +298,22 @@ namespace zooma_api.Controllers
 
             if (extractedEmail == null) return NotFound("Token hết hạn");
 
-            var result = await _context.Users.FirstOrDefaultAsync(row => row.Email == extractedEmail);
+            var result = GetCurrentUser();
 
             return Ok(result);
+        }
+
+        private User GetCurrentUser()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+                var email = userClaims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
+                var result = _context.Users.FirstOrDefault(x => x.Email == email);
+                return result;
+            }
+            return null;
         }
         // End
 
