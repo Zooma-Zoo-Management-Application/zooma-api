d85306e8ef5e0ad9cfc50580e01185bf98dab3be
hmtken <143521414+hmtken@users.noreply.github.com>
2023-09-27 16:21:18 +0700

    Demo source code vnpay ( still error ) (#19)
    Co-authored-by: Le Quoc Uy <91858657+UyLeQuoc@users.noreply.github.com>

---
+++ zooma-api/OrderInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace zooma_api
+{
+    public class OrderInfo
+    {
+        public long OrderId { get; set; }
+        public decimal Amount { get; set; }
+        public string OrderDesc { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ zooma-api/VnPayLibrary.cs
@@ -0,0 +1,44 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace zooma_api
+{
+    public class VnPayLibrary
+    {
+        private SortedList<string, string> _requestData = new SortedList<string, string>();
+
+        public void AddRequestData(string key, string value)
+        {
+            if (!_requestData.ContainsKey(key))
+            {
+                _requestData.Add(key, value);
+            }
+            else
+            {
+                _requestData[key] = value;
+            }
+        }
+
+        public string CreateRequestUrl(string paymentUrl, string secretKey)
+        {
+            StringBuilder data = new StringBuilder();
+            foreach (KeyValuePair<string, string> kv in _requestData)
+            {
+                if (!string.IsNullOrEmpty(kv.Value))
+                {
+                    data.Append(kv.Key + "=" + HttpUtility.UrlEncode(kv.Value) + "&");
+                }
+            }
+
+            string queryString = data.ToString().Remove(data.Length - 1, 1);
+            string rawData = queryString + secretKey;
+
+            SHA256 sha256 = SHA256.Create();
+            byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+            string secureHash = BitConverter.ToString(bytes).Replace("-", "").ToLower();
+
+            return paymentUrl + "?" + queryString + "&vnp_SecureHashType=SHA256&vnp_SecureHash=" + secureHash;
+        }
+    }
+}
