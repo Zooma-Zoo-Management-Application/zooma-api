7f20300e761f19e8f313097903b3187a86e739c7
hmtken <143521414+hmtken@users.noreply.github.com>
2023-09-28 10:10:12 +0700

    Add more function to VnPayLibrary ( can get IP address of user) and create response model ( cant test ) (#20)

--- zooma-api/VnPayLibrary.cs
+++ zooma-api/VnPayLibrary.cs
@@ -1,8 +1,10 @@
 ﻿using System.Globalization;
 using System.Net;
+using System.Net.Sockets;
 using System.Security.Cryptography;
 using System.Text;
 using System.Web;
+using zooma_api.DTO;
 
 namespace zooma_api
 {
@@ -12,7 +14,12 @@ namespace zooma_api
         private SortedList<String, String> _requestData = new SortedList<String, String>(new VnPayCompare());
         private SortedList<String, String> _responseData = new SortedList<String, String>(new VnPayCompare());
 
-        public void AddRequestData(string key, string value)
+
+        // ============================== XỬ LÝ REQUEST THANH TOÁN URL =======================//
+
+        // -- START -- //
+
+        public void AddRequestData(string key, string value) // add vào url
         {
             if (!String.IsNullOrEmpty(value))
             {
@@ -20,28 +27,7 @@ namespace zooma_api
             }
         }
 
-        public void AddResponseData(string key, string value)
-        {
-            if (!String.IsNullOrEmpty(value))
-            {
-                _responseData.Add(key, value);
-            }
-        }
-
-        public string GetResponseData(string key)
-        {
-            string retValue;
-            if (_responseData.TryGetValue(key, out retValue))
-            {
-                return retValue;
-            }
-            else
-            {
-                return string.Empty;
-            }
-        }
-
-        public string CreateRequestUrl(string paymentUrl, string secretKey)
+        public string CreateRequestUrl(string paymentUrl, string secretKey) // METHOD TẠO URL THANH TOÁN
         {
             StringBuilder data = new StringBuilder();
             foreach (KeyValuePair<string, string> kv in _requestData)
@@ -64,6 +50,127 @@ namespace zooma_api
             paymentUrl += "vnp_SecureHash=" + vnp_SecureHash;
 
             return paymentUrl;
+        }
+        public string GetIpAddress(HttpContext context)
+        {
+            var ipAddress = string.Empty;
+            try
+            {
+                var remoteIpAddress = context.Connection.RemoteIpAddress;
+
+                if (remoteIpAddress != null)
+                {
+                    if (remoteIpAddress.AddressFamily == AddressFamily.InterNetworkV6)
+                    {
+                        remoteIpAddress = Dns.GetHostEntry(remoteIpAddress).AddressList
+                            .FirstOrDefault(x => x.AddressFamily == AddressFamily.InterNetwork);
+                    }
+
+                    if (remoteIpAddress != null) ipAddress = remoteIpAddress.ToString();
+
+                    return ipAddress;
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+
+            return "127.0.0.1";
+        }
+
+        // -- END -- //
+
+
+
+        // ============================== XỬ LÝ RESPONSE DATA =======================//
+        // -- START -- //
+
+        public void AddResponseData(string key, string value) // trả về dữ liệu
+        {
+            if (!String.IsNullOrEmpty(value))
+            {
+                _responseData.Add(key, value);
+            }
+        }
+
+        public string GetResponseData(string key)
+        {
+            return _responseData.TryGetValue(key, out var retValue) ? retValue : string.Empty;
+
+        }
+        public PaymentResponseModel GetFullResponseData(IQueryCollection collection, string hashSecret)
+        {
+            var vnPay = new VnPayLibrary();
+
+            foreach (var (key, value) in collection)
+            {
+                if (!string.IsNullOrEmpty(key) && key.StartsWith("vnp_"))
+                {
+                    vnPay.AddResponseData(key, value);
+                }
+            }
+
+            var orderId = Convert.ToInt64(vnPay.GetResponseData("vnp_TxnRef")); //chuyển c
+            var vnPayTranId = Convert.ToInt64(vnPay.GetResponseData("vnp_TransactionNo"));
+            var vnpResponseCode = vnPay.GetResponseData("vnp_ResponseCode");
+            var vnpSecureHash =
+                collection.FirstOrDefault(k => k.Key == "vnp_SecureHash").Value; //hash của dữ liệu trả về
+            var orderInfo = vnPay.GetResponseData("vnp_OrderInfo");
+
+            var checkSignature =
+                vnPay.ValidateSignature(vnpSecureHash, hashSecret); //check Signature
+
+            if (!checkSignature)
+                return new PaymentResponseModel()
+                {
+                    Success = false
+                };
+
+            return new PaymentResponseModel()
+            {
+                Success = true,
+                PaymentMethod = "VnPay",
+                OrderDescription = orderInfo,
+                OrderId = orderId.ToString(),
+                PaymentId = vnPayTranId.ToString(),
+                TransactionId = vnPayTranId.ToString(),
+                Token = vnpSecureHash,
+                VnPayResponseCode = vnpResponseCode
+            };
+        }
+        public bool ValidateSignature(string inputHash, string secretKey)
+        {
+            var rspRaw = GetResponseData();
+            var myChecksum = Utils.HmacSHA512(secretKey, rspRaw);
+            return myChecksum.Equals(inputHash, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        private string GetResponseData()
+        {
+            var data = new StringBuilder();
+            if (_responseData.ContainsKey("vnp_SecureHashType"))
+            {
+                _responseData.Remove("vnp_SecureHashType");
+            }
+
+            if (_responseData.ContainsKey("vnp_SecureHash"))
+            {
+                _responseData.Remove("vnp_SecureHash");
+            }
+
+            foreach (var (key, value) in _responseData.Where(kv => !string.IsNullOrEmpty(kv.Value)))
+            {
+                data.Append(WebUtility.UrlEncode(key) + "=" + WebUtility.UrlEncode(value) + "&");
+            }
+
+            //remove last '&'
+            if (data.Length > 0)
+            {
+                data.Remove(data.Length - 1, 1);
+            }
+
+            return data.ToString();
         }
     }
 
