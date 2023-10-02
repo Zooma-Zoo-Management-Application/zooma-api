4945e2aba733ec5ae7e36a4b9e293663272b1fa8
hmtken <minhtien11052003@gmail.com>
2023-10-02 23:38:40 +0700

    receive Url from VnPay and create transaction

---
+++ zooma-api/Controllers/TransactionDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace zooma_api.Controllers
+{
+    public class TransactionDTO
+    {
+        public DateTime Date { get; set; }
+        public string? AccountNumber { get; set; }
+        public string? TransactionToken { get; set; }
+        public double AmountOfMoney { get; set; }
+        public bool Status { get; set; }
+        public int OrderId { get; set; }
+    }
+}
