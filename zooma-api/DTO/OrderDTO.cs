aa9593e21df7701103c1b9a4d797cb4c610c9c96
hmtken <143521414+hmtken@users.noreply.github.com>
2023-10-03 20:53:15 +0700

    Order Management ( history, update on user), update ticket price. (#29)

---
+++ zooma-api/DTO/OrderDTO.cs
@@ -0,0 +1,23 @@
+﻿using zooma_api.Models;
+
+namespace zooma_api.DTO
+{
+    public class OrderDTO
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public float TotalPrice { get; set; }
+        public string? Notes { get; set; }
+        public string PaymentMethod { get; set; } = null!;
+        public bool Status { get; set; }
+        public short UserId { get; set; }
+
+        // trả về kèm thêm
+        public UserDTO User { get; set; } = null!;
+        public ICollection<OrderDetailDTO> OrderDetails { get; set; } = new List<OrderDetailDTO>();
+        public  ICollection<TransactionDTO> Transactions { get; set; } = new List<TransactionDTO>();
+
+
+
+    }
+}
