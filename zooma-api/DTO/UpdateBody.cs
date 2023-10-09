3e24b9753c1e183372659c767934ea192413bc09
hmtken <minhtien11052003@gmail.com>
2023-10-09 15:40:56 +0700

    Complete Account Mangement

--- zooma-api/DTO/UpdateBody.cs
+++ zooma-api/DTO/UpdateBody.cs
@@ -2,6 +2,7 @@
 {
     public class UpdateUserBody
     {
+        public short id {  get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
         public string FullName { get; set; }
