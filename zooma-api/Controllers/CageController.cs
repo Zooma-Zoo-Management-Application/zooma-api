e181bdfbb7a125bb39350766e9b3bb7c3a3b9667
tk100823 <91471595+trungkien100823@users.noreply.github.com>
2023-10-12 16:12:17 +0700

    re-add Area,Cage, Diet controller

---
+++ zooma-api/Controllers/CageController.cs
@@ -0,0 +1,224 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using zooma_api.DTO;
+using zooma_api.Models;
+
+namespace zooma_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CageController : ControllerBase
+    {
+        private readonly zoomadbContext _context;
+
+        public CageController(zoomadbContext context)
+        {
+            _context = context;
+        }
+        //Hàm lấy tất cả cage
+        [HttpGet("GetAllCages")]
+        public async Task<ActionResult<IEnumerable<CagesDTO>>> GetAllCages()
+        {
+            if (_context.Cages == null)
+            {
+                return NotFound();
+            }
+            var cagesDTO = _context.Cages.Select(c => new CagesDTO
+            {
+                Id = c.Id,
+                Name = c.Name,
+                AnimalLimit = c.AnimalLimit,
+                AnimalCount = c.AnimalCount,
+                Description = c.Description,
+                Status = c.Status,
+                AreaId = c.AreaId
+            });
+            return Ok(cagesDTO);
+        }
+        //Get cage by ID 
+        [HttpGet("GetCageById/{id}")]
+        public async Task<ActionResult<CagesDTO>> GetCageById(short id)
+        {
+            if (_context.Cages == null)
+            {
+                return NotFound();
+            }
+            var cage = await _context.Cages.FindAsync(id);
+            if (cage == null)
+            {
+                return NotFound();
+            }
+            return Ok(cage);
+        }
+
+
+        //Hàm tạo cage mới
+        [HttpPost("CreateCage")]
+        public async Task<ActionResult<CagesDTO>> CreateCage(CagesDTO createCage)
+        {
+            if (_context.Cages == null)
+            {
+                return NotFound();
+            }
+            var cage = new Cage
+            {
+                Name = createCage.Name,
+                AnimalLimit = createCage.AnimalLimit,
+                AnimalCount = createCage.AnimalCount,
+                Description = createCage.Description,
+                Status = createCage.Status,
+                AreaId = createCage.AreaId
+            };
+            _context.Cages.Add(cage);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCage", new { id = cage.Id }, cage);
+        }
+        //select an animal from the database and assign to the cage 
+        [HttpPut("AssignAnimal/{id}/{cageID}")]
+        public async Task<IActionResult> AssignAnimal(int id, int cageID)
+        {
+            var animal = await _context.Animals.FindAsync(id);
+            var cageDTO = await _context.Cages.FindAsync(cageID);
+            if (animal == null || cageDTO == null)
+            {
+                return NotFound("No animals available");
+            }
+            if (cageDTO.AnimalCount < cageDTO.AnimalLimit)
+            {
+                cageDTO.AnimalCount++;
+                animal.CageId = (short?)cageID;
+                await _context.SaveChangesAsync();
+                return Ok("Animal assigned to cage");
+            }
+            else
+            {
+                return BadRequest("Cage is full");
+            }
+        }
+
+
+
+        //  Animal animalsByName = await _context.Animals.Where(a => a.Name.Contains(name));
+
+
+        //Hàm xóa cage
+        [HttpDelete("DeleteCage/{id}")]
+        public async Task<IActionResult> DeleteCage(int id)
+        {
+            if (_context.Cages == null)
+            {
+                return Problem("Entity set 'ZoomaContext.Cages'  is null.");
+            }
+            var cage = await _context.Cages.FindAsync(id);
+            if (cage != null)
+            {
+                _context.Cages.Remove(cage);
+            }
+
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+
+        // GET: api/Cage
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cage>>> GetCages()
+        {
+            if (_context.Cages == null)
+            {
+                return NotFound();
+            }
+            return await _context.Cages.ToListAsync();
+        }
+
+        // GET: api/Cage/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Cage>> GetCage(short id)
+        {
+            if (_context.Cages == null)
+            {
+                return NotFound();
+            }
+            var cage = await _context.Cages.FindAsync(id);
+
+            if (cage == null)
+            {
+                return NotFound();
+            }
+
+            return cage;
+        }
+
+        // PUT: api/Cage/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCage(short id, Cage cage)
+        {
+            if (id != cage.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(cage).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CageExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Cage
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Cage>> PostCage(Cage cage)
+        {
+            if (_context.Cages == null)
+            {
+                return Problem("Entity set 'zoomadbContext.Cages'  is null.");
+            }
+            _context.Cages.Add(cage);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCage", new { id = cage.Id }, cage);
+        }
+
+        // DELETE: api/Cage/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCage(short id)
+        {
+            if (_context.Cages == null)
+            {
+                return NotFound();
+            }
+            var cage = await _context.Cages.FindAsync(id);
+            if (cage == null)
+            {
+                return NotFound();
+            }
+
+            _context.Cages.Remove(cage);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CageExists(short id)
+        {
+            return (_context.Cages?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ zooma-api/Controllers/DietController.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using zooma_api.Models;
+
+namespace zooma_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DietController : ControllerBase
+    {
+        private readonly zoomadbContext _context;
+
+        public DietController(zoomadbContext context)
+        {
+            _context = context;
+        }
+        //Get all diets in the system
+        [HttpGet("GetAllDiets")]
+        public async Task<ActionResult<IEnumerable<Diet>>> GetAllDiets()
+        {
+            if (_context.Diets == null)
+            {
+                return Problem("Entity set 'ZoomaContext.Diets'  is null.");
+            }
+            return await _context.Diets.ToListAsync();
+        }
+        //Get detailed information of a diet
+        [HttpGet("GetDietsByID/{id}")]
+        public async Task<ActionResult<Diet>> GetDietByID(int id)
+        {
+            if (_context.Diets == null)
+            {
+                return Problem("Entity set 'ZoomaContext.Diets'  is null.");
+            }
+            var diet = await _context.Diets.FindAsync(id);
+
+            if (diet == null)
+            {
+                return NotFound();
+            }
+
+            return diet;
+        }
+
+
+        // Create a new diet
+        [HttpPost("CreateDiets")]
+        public async Task<ActionResult<Diet>> CreateDiet(Diet diet)
+        {
+            if (_context.Diets == null)
+            {
+                return Problem("Entity set 'ZoomaContext.Diets'  is null.");
+            }
+            _context.Diets.Add(diet);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDiet", new { id = diet.Id }, diet);
+        }
+        //Update a diet
+        [HttpPut("UpdateDiets/{id}")]
+        public async Task<IActionResult> UpdateDiet(int id, Diet diet)
+        {
+            if (id != diet.Id)
+            {
+                return BadRequest("No diets with that ID");
+            }
+
+            _context.Entry(diet).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException) when (!DietExists(id))
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        // GET: api/Diet
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Diet>>> GetDiets()
+        {
+            if (_context.Diets == null)
+            {
+                return NotFound();
+            }
+            return await _context.Diets.ToListAsync();
+        }
+
+        // GET: api/Diet/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Diet>> GetDiet(int id)
+        {
+            if (_context.Diets == null)
+            {
+                return NotFound();
+            }
+            var diet = await _context.Diets.FindAsync(id);
+
+            if (diet == null)
+            {
+                return NotFound();
+            }
+
+            return diet;
+        }
+
+        // PUT: api/Diet/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDiet(int id, Diet diet)
+        {
+            if (id != diet.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(diet).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DietExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Diet
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Diet>> PostDiet(Diet diet)
+        {
+            if (_context.Diets == null)
+            {
+                return Problem("Entity set 'zoomadbContext.Diets'  is null.");
+            }
+            _context.Diets.Add(diet);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDiet", new { id = diet.Id }, diet);
+        }
+
+        // DELETE: api/Diet/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDiet(int id)
+        {
+            if (_context.Diets == null)
+            {
+                return NotFound();
+            }
+            var diet = await _context.Diets.FindAsync(id);
+            if (diet == null)
+            {
+                return NotFound();
+            }
+
+            _context.Diets.Remove(diet);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool DietExists(int id)
+        {
+            return (_context.Diets?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
