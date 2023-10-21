89d29a056e8176d5f8315f63cdf803d4c1c62b5d
wangchen <hoangtien01052003@gmail.com>
2023-10-21 14:02:44 +0700

    Modify MappingProfiles, fix name convention of AnimalTrainersController Create TypesController, TypeDTO

---
+++ zooma-api/DTO/TypeDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace zooma_api.DTO
+{
+    public class TypeDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public bool Status { get; set; }
+    }
+}
