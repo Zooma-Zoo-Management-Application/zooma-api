614e6040ec2192425c3ea71827c1c2a1e6ccda81
wangchen <hoangtien01052003@gmail.com>
2023-10-25 13:10:41 +0700

    Modify DietsController, DietDTO

--- zooma-api/Controllers/DietsController.cs
+++ zooma-api/Controllers/DietsController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using zooma_api.DTO;
 using zooma_api.Models;
 
 namespace zooma_api.Controllers
@@ -8,40 +10,55 @@ namespace zooma_api.Controllers
     [ApiController]
     public class DietsController : ControllerBase
     {
-        private readonly zoomadbContext _context;
+        public zoomadbContext _context = new zoomadbContext();
+        private readonly IMapper _mapper;
 
-        public DietsController(zoomadbContext context)
+        public DietsController(zoomadbContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
 
-        // GET: api/Diets
-        [HttpGet("GetDiets")]
-        public async Task<ActionResult<IEnumerable<Diet>>> GetDiets()
+        //Hàm lấy tất cả các diet
+        [HttpGet()]
+        public async Task<ActionResult<IEnumerable<DietDTO>>> GetDiets()
         {
             if (_context.Diets == null)
             {
                 return NotFound();
             }
-            return await _context.Diets.ToListAsync();
+
+            var diet = await _context.Diets.Include(a => a.Animals).ToListAsync();
+            
+            if (diet == null)
+            {
+                return NotFound("No diet is available");
+            }
+            
+            var dietDTO = _mapper.Map<List<DietDTO>>(diet);
+
+            return dietDTO;
         }
 
-        // GET: api/Diets/5
-        [HttpGet("GetDietByID/{id}")]
-        public async Task<ActionResult<Diet>> GetDiet(int id)
+        // Hàm lấy Diet dựa trên Id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DietDTO>> GetDietById(int id)
         {
             if (_context.Diets == null)
             {
                 return NotFound();
             }
-            var diet = await _context.Diets.FindAsync(id);
+
+            var diet = await _context.Diets.Include(a => a.Animals).FirstOrDefaultAsync(e => e.Id == id);
 
             if (diet == null)
             {
-                return NotFound();
+                return NotFound("There is no diet match your found");
             }
 
-            return diet;
+            var dietDTO = _mapper.Map<DietDTO>(diet);
+
+            return dietDTO;
         }
         // GET: api/Diets/5
         [HttpGet("GetDietByName/{name}")]
@@ -61,72 +78,94 @@ namespace zooma_api.Controllers
             return diet;
         }
 
-        // PUT: api/Diets/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("UpdateDiet/{id}")]
-        public async Task<IActionResult> PutDiet(int id, DietUpdate diet)
+        //Hàn update diet
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateDiet(int id, DietUpdate dietUpdate)
         {
-            var dietUpdate = await _context.Diets.FindAsync(id);
-            if (dietUpdate == null)
-            {
-                return BadRequest();
-            }
-            dietUpdate.Name = dietUpdate.Name ?? diet.Name;
-            dietUpdate.Description = dietUpdate.Description ?? diet.Description;
-            dietUpdate.CreateAt = dietUpdate.CreateAt;
-            dietUpdate.UpdateAt = dietUpdate.UpdateAt;
-            dietUpdate.ScheduleAt = dietUpdate.ScheduleAt;
-            dietUpdate.Status = dietUpdate.Status;
-            dietUpdate.Goal = dietUpdate.Goal ?? diet.Goal;
-            dietUpdate.EndAt = dietUpdate.EndAt;
-            dietUpdate.TotalEnergyValue = dietUpdate.TotalEnergyValue;
+            var diet = await _context.Diets.FirstOrDefaultAsync(e => e.Id == id);
 
-
-            try
+            if (diet == null)
             {
-                await _context.SaveChangesAsync();
+                return NotFound("Can't found this diet");
             }
-            catch (DbUpdateConcurrencyException)
+            else
             {
-                if (!DietExists(id))
+                var dietDetailsId = _context.DietDetails.Where(e => e.DietId == diet.Id).Include(e => e.Food).ToList();
+                
+                double totalEnergy = 0;
+
+                foreach (var item in dietDetailsId)
                 {
-                    return NotFound();
+                    if (item != null && item.Food != null)
+                    {
+                        totalEnergy += item.Food.EnergyValue * item.FeedingInterval;
+                    }
                 }
-                else
-                {
-                    throw;
-                }
-            }
 
-            return Ok(dietUpdate);
+                diet.Name = dietUpdate.Name ?? diet.Name;
+                diet.Description = dietUpdate.Description;
+                //        dietUpdate.CreateAt = dietUpdate.CreateAt;
+                diet.UpdateAt = dietUpdate.UpdateAt;
+                diet.ScheduleAt = dietUpdate.ScheduleAt;
+                diet.Status = dietUpdate.Status;
+                diet.Goal = dietUpdate.Goal;
+                diet.EndAt = dietUpdate.EndAt;
+                diet.TotalEnergyValue = totalEnergy;
+
+                try
+                {
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DietExists(id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+
+                return Ok("Update successfully");
+
+            }
         }
 
-        // POST: api/Diets
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost("CreateDiet")]
-        public async Task<ActionResult<Diet>> PostDiet(DietUpdate dietUpdate)
+        // Hàm tạo diet mới
+        [HttpPost()]
+        public async Task<ActionResult<Diet>> CreateDiet(DietCreate dietCreate)
         {
             if (_context.Diets == null)
             {
                 return NotFound();
             }
+
             Diet diet = new Diet
             {
-                Name = dietUpdate.Name,
-                Description = dietUpdate.Description,
-                CreateAt = dietUpdate.CreateAt,
-                UpdateAt = dietUpdate.UpdateAt,
-                ScheduleAt = dietUpdate.ScheduleAt,
-                Status = dietUpdate.Status,
-                Goal = dietUpdate.Goal,
-                EndAt = dietUpdate.EndAt,
-                TotalEnergyValue = dietUpdate.TotalEnergyValue
+                Name = dietCreate.Name,
+                Description = dietCreate.Description,
+                CreateAt = dietCreate.CreateAt,
+                UpdateAt = dietCreate.UpdateAt,
+                ScheduleAt = dietCreate.ScheduleAt,
+                Status = false,
+                Goal = dietCreate.Goal,
+                EndAt = dietCreate.EndAt,
+                TotalEnergyValue = 0
             };
+
+            var dietExists = _context.Diets.FirstOrDefault(e => e.Name == diet.Name);
+
+            if(dietExists != null)
+            {
+                return BadRequest("This diets is existed");
+            }
 
             _context.Diets.Add(diet);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetDiet", new { id = diet.Id }, diet);
+            return Ok(new {dietDTO = _mapper.Map<DietDTO>(diet), message = "Create successfully!"});
 
         }
 
@@ -134,31 +173,27 @@ namespace zooma_api.Controllers
         {
             return (_context.Diets?.Any(e => e.Id == id)).GetValueOrDefault();
         }
+        
+
         public class DietUpdate
         {
             public string Name { get; set; } = null!;
             public string? Description { get; set; }
-            public DateTime CreateAt { get; set; }
             public DateTime UpdateAt { get; set; }
             public DateTime ScheduleAt { get; set; }
             public bool Status { get; set; }
             public string Goal { get; set; } = null!;
             public DateTime EndAt { get; set; }
-            public double TotalEnergyValue { get; set; }
         }
-        public class DietBody
+        public class DietCreate
         {
-            public string Name { get; set; } = null!;
+            public string Name { get; set; }
             public string? Description { get; set; }
             public DateTime CreateAt { get; set; }
             public DateTime UpdateAt { get; set; }
             public DateTime ScheduleAt { get; set; }
-            public bool Status { get; set; }
-            public string Goal { get; set; } = null!;
+            public string Goal { get; set; }
             public DateTime EndAt { get; set; }
-            public double TotalEnergyValue { get; set; }
-
         }
-
     }
 }
