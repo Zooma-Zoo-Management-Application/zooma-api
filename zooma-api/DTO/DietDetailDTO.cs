b293e7b2bb34bf6cf5d3c181f2da9ee18ba5fe5f
wangchen <hoangtien01052003@gmail.com>
2023-11-01 17:23:17 +0700

    Modify DietDetailsController

--- zooma-api/DTO/DietDetailDTO.cs
+++ zooma-api/DTO/DietDetailDTO.cs
@@ -1,4 +1,6 @@
-﻿namespace zooma_api.DTO
+﻿using System.Text.Json.Serialization;
+
+namespace zooma_api.DTO
 {
     public class DietDetailDTO
     {
@@ -9,10 +11,13 @@
         public DateTime UpdateAt { get; set; }
         public DateTime? ScheduleAt { get; set; }
         public DateTime? EndAt { get; set; }
-        public short FeedingInterval { get; set; }
+        [JsonIgnore]
+        public string? FeedingDate { get; set; }
+        public TimeSpan? FeedingTime { get; set; }
         public bool Status { get; set; }
         public int DietId { get; set; }
         public int FoodId { get; set; }
+        public string[] FeedingDateArray { get; set; }
         public DietDTO diet { get; set; }
         public FoodDTO food { get; set; }   
     }
