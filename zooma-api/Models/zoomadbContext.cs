49195937446543cdfe493ab68eb7baa07bc7d021
satoshiuy <lequocuyit@gmail.com>
2023-11-06 12:56:15 +0700

    update connection string

--- zooma-api/Models/zoomadbContext.cs
+++ zooma-api/Models/zoomadbContext.cs
@@ -44,7 +44,7 @@ namespace zooma_api.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=tcp:zooma-db.database.windows.net,1433;Initial Catalog=Zooma;Persist Security Info=False;User ID=zooma-db;Password=huydiet@SWP;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                optionsBuilder.UseSqlServer("Server=tcp:zooma.database.windows.net,1433;Initial Catalog=zoomaDB;Persist Security Info=False;User ID=zooma;Password=huydiet@SWP;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
         }
 
